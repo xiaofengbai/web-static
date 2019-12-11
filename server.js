362ff0fc20da68e38e63692eb9a152e1e8f4d1c3
baixiaofeng <xiaofeng.bai@unicareer.com>
2019-12-11 16:20:15 +0800

    修改端口

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-var server = app.listen(3002, function() {
+var server = app.listen(9991, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log("应用实例，访问地址为 http://%s:%s", host, port);
